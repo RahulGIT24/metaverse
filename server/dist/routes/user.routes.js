db766210e9f1ff1cbc8135e148ac879e5b4bd571
rahul <rg4005450@gmail.com>
2025-01-18 00:37:50 +0530

    dist removed

--- server/dist/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../middleware/auth");
-const user_controller_1 = require("../controllers/user.controller");
-const router = (0, express_1.Router)();
-router.get("/get-current-user", auth_1.verifyJWT, user_controller_1.getUser);
-exports.default = router;
